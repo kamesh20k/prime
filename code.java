eadc29c4dc93d8d54d335aa71cef26efe57bd47c
kamesh20k <96664751+kamesh20k@users.noreply.github.com>
2023-02-13 09:42:20 +0530

    6thcommit

--- code.java
+++ code.java
@@ -7,7 +7,7 @@ char ch1 = 'd';
 char ch2 = 'e';  
 // variable that stores the integer value of the character  
 int asciivalue1 = ch1;  
-int asciivalue2 = ch2;  
+int asciivalue2 = ch3;  
 System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
 System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
 }  
