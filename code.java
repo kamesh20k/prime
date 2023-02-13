6ab119a8ecc18aba7af2644e7dad8b66e1db51b6
kamesh20k <96664751+kamesh20k@users.noreply.github.com>
2023-02-13 09:45:42 +0530

    7thcommit-dharani

--- code.java
+++ code.java
@@ -9,6 +9,6 @@ char ch2 = 'e';
 int asciivalue1 = ch1;  
 int asciivalue2 = ch3;  
 System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
-System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+System.out.println("The ASCII value of " + ch3 + " is: " + asciivalue2);  
 }  
 }  
